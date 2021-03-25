fbf714a75e1a672526fae725dfd2430ce908ef69
BasitAhmad <basitahmad4@gmail.com>
2021-03-25 23:26:48 +0530

    260756-Implementation of complex_calc

---
+++ main.c
@@ -0,0 +1,25 @@
+#include "stdio.h"
+#include "inc\complex_calculator.h"
+
+int main(){
+    complex_t a = {1, 1}, b = {3, 1}, c = {0, 0};
+    
+    complex_sum(&a, &b, &c);
+    printf("%f %f\n\n", c.real, c.imaginary);
+    
+    c.real = 0.0;
+    c.imaginary = 0.0;
+    complexSub(&a, &b, &c);
+    printf("%f %f\n\n", c.real, c.imaginary);
+
+    c.real = 0.0;
+    c.imaginary = 0.0;
+    complexMul(&a, &b, &c);
+    printf("%f %f\n\n", c.real, c.imaginary);
+
+    c.real = 0.0;
+    c.imaginary = 0.0;
+    complexDiv(&a, &b, &c);
+    printf("%f %f\n\n", c.real, c.imaginary);
+    return 0;
+}
